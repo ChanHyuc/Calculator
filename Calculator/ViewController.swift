04f0a18c4f895d27fdf32f9934748a94fed6a2cd
ChanHyuc <whk1140@gmail.com>
2024-09-27 23:15:08 +0900

    Init: Calculator 프로젝트 생성

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by ChanHyuc on 9/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+
+}
+
